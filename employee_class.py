865952a01d4046cbcecb390ed0e4d8d7d2232e8b
Aia Rupsom <aia.rupsom@interacso.com>
2021-11-12 16:02:23 +0200

    starter code for attendance system

---
+++ employee_class.py
@@ -0,0 +1,19 @@
+class Employee(object):
+  def __init__(self, employee_id, name, age, phone):
+    self.employee_id = employee_id
+    self.name = name
+    self.age = age
+    self.phone = phone
+    self.attendance = []
+
+  def add_employee_from_file(self):
+    pass
+
+  def add_employee_manually(self):
+    pass
+
+  def delete_employee_from_file(self):
+    pass
+
+  def delete_employee_manually(self):
+    pass
