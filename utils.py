59fe5b854824ef6a5ae87d7d06de3908691bff09
Aia Rupsom <aiale88@gmail.com>
2021-11-20 17:22:34 +0200

    Add a function to remove from file

--- utils.py
+++ utils.py
@@ -10,3 +10,13 @@ def search_in_file(file_name, condition):
 def add_to_file(file_name, data):
   with open(file_name, "w") as file:
     file.write(data)
+
+
+def remove_from_file(file_name, data):
+  with open(file_name) as file:
+    lines = file.readlines()
+    file.seek(0)
+    for line in lines:
+      if data not in line:
+        file.write(line)
+    file.truncate()
